e5c03dccb9e8c16a26721f5debc32370fd41304c
Redth <jondick@gmail.com>
2017-12-05 09:57:27 -0500

    Added tests and fixture for them

---
+++ src/Cake.UrlLoadDirective.Module.Tests/Fixture.cs
@@ -0,0 +1,62 @@
+﻿using Cake.Core;
+using Cake.Core.Configuration;
+using Cake.Core.Diagnostics;
+using Cake.Core.IO;
+using Cake.Core.Packaging;
+using Cake.Testing;
+using NSubstitute;
+using System.Collections.Generic;
+using Cake.Core.Scripting.Analysis;
+using Cake.Core.Scripting.Processors.Loading;
+
+namespace Cake.UrlLoadDirective.Module.Tests
+{
+	internal sealed class UrlLoadDirectiveProviderFixture
+	{
+		public ICakeEnvironment Environment { get; set; }
+		public IFileSystem FileSystem { get; set; }
+		public IProcessRunner ProcessRunner { get; set; }
+		public IScriptAnalyzerContext ScriptAnalyzerContext { get; set; }
+		public ICakeLog Log { get; set; }
+		public ICakeConfiguration Config { get; set; }
+		public Dictionary<string, string> LoadedReferences { get; set; }
+
+		internal UrlLoadDirectiveProviderFixture()
+		{
+			Environment = FakeEnvironment.CreateUnixEnvironment();
+			FileSystem = new FakeFileSystem(Environment);
+			ProcessRunner = Substitute.For<IProcessRunner>();
+
+			LoadedReferences = new Dictionary<string, string>();
+
+			ScriptAnalyzerContext = Substitute.For<IScriptAnalyzerContext>();
+			ScriptAnalyzerContext.Root.Returns(new FilePath(System.IO.Directory.GetCurrentDirectory()));
+			ScriptAnalyzerContext
+				.WhenForAnyArgs(x => x.Analyze(Arg.Any<FilePath>()))
+				.Do(c => {
+					var fp = c.Arg<FilePath>();
+					LoadedReferences[fp.FullPath] = System.IO.File.ReadAllText(fp.FullPath);
+				});
+
+			Log = new FakeLog();
+			Config = new CakeConfiguration (new Dictionary<string, string> ());
+		}
+
+		internal UrlLoadDirectiveProvider CreateProvider()
+		{
+			return new UrlLoadDirectiveProvider(Config, Environment);
+		}
+
+		internal bool CanLoad(LoadReference loadRef)
+		{
+			var p = CreateProvider();
+			return p.CanLoad(ScriptAnalyzerContext, loadRef);
+		}
+
+		internal void Load(LoadReference loadRef)
+		{
+			var p = CreateProvider();
+			p.Load(ScriptAnalyzerContext, loadRef);
+		}
+	}
+}
--- src/Cake.UrlLoadDirective.Module.Tests/Tests.cs
+++ src/Cake.UrlLoadDirective.Module.Tests/Tests.cs
@@ -1,13 +1,59 @@
 ﻿using System;
+using Cake.Core.Scripting.Processors.Loading;
 using Xunit;
 
 namespace Cake.UrlLoadDirective.Module.Tests
 {
 	public class Tests
 	{
-		[Fact]
-		public void Test ()
+		[Theory]
+		[InlineData("url:https://raw.githubusercontent.com/Redth/Cake.UrlLoadDirective.Module/master/src/Cake.UrlLoadDirective.Module.Tests/test.cake")]
+		[InlineData("url:http://raw.githubusercontent.com/Redth/Cake.UrlLoadDirective.Module/master/src/Cake.UrlLoadDirective.Module.Tests/test.cake")]
+		public void Should_CanLoad (string url)
 		{
+			var f = new UrlLoadDirectiveProviderFixture();
+
+			var lr = new LoadReference(new Uri(url));
+
+			var r = f.CanLoad(lr);
+
+			Assert.True(r);
+		}
+
+
+		[Theory]
+		[InlineData("https://raw.githubusercontent.com/Redth/Cake.UrlLoadDirective.Module/master/src/Cake.UrlLoadDirective.Module.Tests/test.cake")]
+		[InlineData("nuget:?package=Cake.FileHelpers")]
+		public void Should_Not_CanLoad(string url)
+		{
+			var f = new UrlLoadDirectiveProviderFixture();
+
+			var lr = new LoadReference(new Uri(url));
+
+			var r = f.CanLoad(lr);
+
+			Assert.False(r);
+		}
+
+
+
+		[Theory]
+		[InlineData("url:https://raw.githubusercontent.com/Redth/Cake.UrlLoadDirective.Module/master/src/Cake.UrlLoadDirective.Module.Tests/test.cake")]
+		[InlineData("url:http://raw.githubusercontent.com/Redth/Cake.UrlLoadDirective.Module/master/src/Cake.UrlLoadDirective.Module.Tests/test.cake")]
+		public void Load_Url_Reference(string url)
+		{
+			var f = new UrlLoadDirectiveProviderFixture();
+
+			var lr = new LoadReference(new Uri(url));
+
+			f.Load(lr);
+
+
+			Assert.NotEmpty(f.LoadedReferences);
+
+			foreach (var k in f.LoadedReferences.Keys)
+				Console.WriteLine(k);
+			
 		}
 	}
 }
